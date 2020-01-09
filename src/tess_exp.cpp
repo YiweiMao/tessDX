c2137e3bce137f78dd477d896514f312fd6100e5
Yiwei Mao <46410481+YiweiMao@users.noreply.github.com>
2020-01-09 17:43:05 +1100

    Add files via upload

--- src/tess_exp.cpp
+++ src/tess_exp.cpp
@@ -1,30 +1,59 @@
 #include <stdio.h>
 #include "tessellator.hpp"
 
-int main(void) {
-    printf("Hello World!\n");
+int main(void) { //int argc, char** argv) {
+
+    int partitionScheme;
+    int outputPrimitive;
+    int domain;
+    int numTFs;
+    float tfs[6] = {0};
+
+    FILE *fp;
+    fp = fopen("settings.txt", "r");
+    fscanf(fp, "%d", &partitionScheme);
+    fscanf(fp, "%d", &outputPrimitive);
+    fscanf(fp, "%d", &domain);
+    fscanf(fp, "%d", &numTFs);
+
+    for (int i=0; i <numTFs; i++) {
+        fscanf(fp, "%f", &tfs[i]);
+    }
+    fclose(fp);
 
     CHWTessellator hwTess;
-    hwTess.Init(D3D11_TESSELLATOR_PARTITIONING_FRACTIONAL_EVEN,D3D11_TESSELLATOR_OUTPUT_TRIANGLE_CW);
-    hwTess.TessellateTriDomain(1, 1, 1, 3);
+    hwTess.Init((D3D11_TESSELLATOR_PARTITIONING)partitionScheme,(D3D11_TESSELLATOR_OUTPUT_PRIMITIVE)outputPrimitive);
 
-    printf("get point count is %d\n",hwTess.GetPointCount());
+    switch (domain)
+    {
+    case 0:
+        hwTess.TessellateIsoLineDomain(tfs[0],tfs[1]);
+        break;
+    case 1:
+        hwTess.TessellateTriDomain(tfs[0],tfs[1],tfs[2],tfs[3]);
+        break;
+    case 2:
+        hwTess.TessellateQuadDomain(tfs[0],tfs[1],tfs[2],tfs[3],tfs[4],tfs[5]);
+        break;
+    default:
+        break;
+    }
+
+    fp = fopen("points.csv", "w");
+    //printf("get point count is %d\n",hwTess.GetPointCount());
     for(int i=0;i<hwTess.GetPointCount();i++){
-        printf("%.2f,%.2f\t",hwTess.m_Point[i].u,hwTess.m_Point[i].v);
+        //printf("%.2f,%.2f\t",hwTess.m_Point[i].u,hwTess.m_Point[i].v);
+        fprintf(fp,"%.16f,%.16f\n",hwTess.m_Point[i].u,hwTess.m_Point[i].v);
     }
+    fclose(fp);
 
-    printf("\nget index count is %d\n",hwTess.GetIndexCount());
-    
+    fp = fopen("indexes.csv", "w");
+    //printf("\nget index count is %d\n",hwTess.GetIndexCount());
     for(int i=0;i<hwTess.GetIndexCount();i++){
-        printf("%d\t",hwTess.m_Index[i]);
+        //printf("%d\t",hwTess.m_Index[i]);
+        fprintf(fp,"%d\n",hwTess.m_Index[i]);
     }
+    fclose(fp);
 
-    hwTess.DumpAllPointsAsInOrderLineList();
-    printf("\nget index count is %d\n",hwTess.GetIndexCount());
-    for(int i=0;i<hwTess.GetIndexCount();i++){
-        printf("%d\t",hwTess.m_Index[i]);
-    }
-
-    printf("\nHello world again");
     return 0;
 }
